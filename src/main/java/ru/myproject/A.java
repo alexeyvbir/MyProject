b1e5005b4bcb0c6982fd8cc7a1f21b451b00e467
Alexey Biryukov <alexeyvbir@gmail.com>
2024-05-15 14:05:06 +0300

    ССС-С123 javacore-16 example

--- src/main/java/ru/myproject/A.java
+++ src/main/java/ru/myproject/A.java
@@ -2,4 +2,12 @@ package ru.myproject;
 
 public class A {
     // безумно важный и интересный код
+    public static void main(String[] args) {
+        double a = Long.MAX_VALUE;
+        long b = Long.MAX_VALUE;
+        int c = 1;
+        System.out.println(a+b+c);
+        System.out.println(c+a+b);
+
+    }
 }

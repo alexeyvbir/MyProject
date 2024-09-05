40ae640798871f847727c8a372815b25ceebb4c6
Alexey Biryukov <alexeyvbir@gmail.com>
2024-09-05 19:15:37 +0300

    CCC-D1234: Fix PR

--- src/main/java/ru/myproject/C.java
+++ src/main/java/ru/myproject/C.java
@@ -9,6 +9,7 @@ public class C {
     public static void main(String[] args) {
         int a = 5;
         int value = 15;
+        int value1 = 125;
         Map<String,String> map = new HashMap<String, String>();
         int k = 7;
 

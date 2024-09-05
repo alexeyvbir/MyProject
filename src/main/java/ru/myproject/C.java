bf7f51ea35c3d1c774726bf30626223de6d693e3
Alexey Biryukov <alexeyvbir@gmail.com>
2024-09-05 18:59:29 +0300

    CCC-D1234: Ref C.java

--- src/main/java/ru/myproject/C.java
+++ src/main/java/ru/myproject/C.java
@@ -1,5 +1,13 @@
 package ru.myproject;
 
+
+import java.util.HashMap;
+import java.util.Map;
+
 public class C {
     // безумно важный нужный код
+    public static void main(String[] args) {
+        int a = 5;
+        Map<String,String> map = new HashMap<String, String>();
+    }
 }

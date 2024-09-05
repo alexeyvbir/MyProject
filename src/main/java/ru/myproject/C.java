5ad396a4ac94e5c7d27c7f66b21d0a7744cb3d32
Alexey Biryukov <alexeyvbir@gmail.com>
2024-09-05 18:40:57 +0300

    CCC-D1234: Ref C.java

--- src/main/java/ru/myproject/C.java
+++ src/main/java/ru/myproject/C.java
@@ -2,4 +2,10 @@ package ru.myproject;
 
 public class C {
     // безумно важный нужный код
+    public static void main(String[] args) {
+        System.out.println("Начинаем изучать работу с git-ом");
+        System.out.println("Продолжаем работу с git-ом");
+        System.out.println("Снова работаем с git'ом");
+        System.out.println("И опять");
+    }
 }

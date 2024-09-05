060ff4bf4c74a8547ff8bf26216af4f7879f3b64
Alexey Biryukov <alexeyvbir@gmail.com>
2024-09-05 19:14:26 +0300

    CCC-D1234: Ref C.java - 1

--- src/main/java/ru/myproject/C.java
+++ src/main/java/ru/myproject/C.java
@@ -8,8 +8,9 @@ public class C {
     // безумно важный нужный код
     public static void main(String[] args) {
         int a = 5;
+        int value = 15;
         Map<String,String> map = new HashMap<String, String>();
         int k = 7;
-        int p = 9;
+
     }
 }

2cfc0b68913e0ccfe9ade46b16439bbad03b3f52
Alexey Biryukov <alexeyvbir@gmail.com>
2024-11-27 19:23:44 +0300

    ССС-С123 Реализовано использование тестов тест3, 4 и 5 в базовом классе

--- src/main/java/ru/myproject/E.java
+++ src/main/java/ru/myproject/E.java
@@ -3,11 +3,11 @@ package ru.myproject;
 public class E {
 
     public static void test1() {
-        System.out.println("Тест1 в классе E");
+        System.out.println("test1 in E class");
     }
 
     public static void test2() {
-        System.out.println("Тест2 в классе E");
+        System.out.println("test2 in E class");
     }
 
     public static void test3() {

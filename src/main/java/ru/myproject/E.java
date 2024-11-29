afadd3eb6f025d467c45cc7ead2891ade9d0e2ff
Alexey Biryukov <alexeyvbir@gmail.com>
2024-11-29 12:29:36 +0300

    ССС-С123 getting ready for Jenkins

--- src/main/java/ru/myproject/E.java
+++ src/main/java/ru/myproject/E.java
@@ -3,23 +3,6 @@ package ru.myproject;
 public class E {
 
     public static void test1() {
-        System.out.println("test1 in E class");
+        System.out.println("очень важный нужный код");
     }
-
-    public static void test2() {
-        System.out.println("test2 in E class");
-    }
-
-    public static void test3() {
-        System.out.println("Тест3 в классе Е");
-    }
-
-    public static void test4() {
-        System.out.println("тест4 в классе Е");
-    }
-
-    public static void test5() {
-        System.out.println("тест5 в классе Е");
-    }
-
 }
--- src/main/java/ru/myproject/HelloWorld.java
+++ src/main/java/ru/myproject/HelloWorld.java
@@ -4,12 +4,5 @@ public class HelloWorld {
 
     public static void main(String[] args) {
         System.out.println("Hello world!");
-        System.out.println("Новое сообщение");
-        E.test1();
-        F.test1();
-        E.test2();
-        E.test3();
-        E.test4();
-        E.test5();
     }
 }

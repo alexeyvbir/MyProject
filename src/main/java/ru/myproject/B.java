faba6ddd7a436b8e6a795d9d219297918688c959
Alexey Biryukov <alexeyvbir@gmail.com>
2025-03-12 18:44:45 +0300

    ССС-С123: Ref: A.java, Add: B.java, F.java

---
+++ src/main/java/ru/myproject/B.java
@@ -0,0 +1,8 @@
+package ru.myproject;
+
+public class B {
+
+    public static void testMethod2() {
+        System.out.println("Test method 2");
+    }
+}
---
+++ src/main/java/ru/myproject/F.java
@@ -0,0 +1,8 @@
+package ru.myproject;
+
+public class F {
+
+    public static void testMethod3() {
+        System.out.println("test method 3");
+    }
+}

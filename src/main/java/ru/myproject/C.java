3ad425725a001501ec857fc565c7495d966f7961
Alexey Biryukov <alexeyvbir@gmail.com>
2025-08-05 18:38:11 +0300

    ССС-С123: Ref A.java, B.java, C.java

---
+++ src/main/java/ru/myproject/C.java
@@ -0,0 +1,10 @@
+package ru.myproject;
+
+public class C {
+    public static void main(String[] args) {
+        System.out.println("важный код-3");
+    }
+    public static void myMethod() {
+        System.out.println("----");
+    }
+}
